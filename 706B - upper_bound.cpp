30f24719c625640648989db0893509f483543ee2
k123cp <44492864+k123cp@users.noreply.github.com>
2019-08-24 11:42:54 +1000

    Create 706B - upper_bound.cpp

---
+++ 706B - upper_bound.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int x[ 100000 ]; //x[n] - price of the nth shop
+
+int main()
+{
+    int m, n, a;
+    cin >> n;
+    for( int i = 0; i < n; i++ )
+        cin >> x[i];
+    cin >> m;
+    for( int i = 0; i < m; i++ )
+    {
+        cin >> a;
+        cout << upper_bound(x, x + n, a) - x << endl;
+    }
+}   //return position of first element greater than m in array x
